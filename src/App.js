2c1ee5f8e618460b9ee4ebd781f4cdfefbcc1c73
LibbyMills <98463154+LibbyMills@users.noreply.github.com>
2023-04-18 11:09:44 +0100

    CSS styling with logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
+import DictionaryLogo from "./DictionaryLogo.png";
 import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-      <section>
-        <Dictionary />
-      </section>
+      <div className="container">
+        <header className="App-header">
+          <img src={DictionaryLogo} className="logo" />
+        </header>
+
+        <section>
+          <Dictionary />
+        </section>
+      </div>
     </div>
   );
 }
